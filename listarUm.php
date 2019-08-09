054d8484e479664d21add1ef66225ce35d482a50
Informática <Informtica@adolphoberezin.com>
2019-08-09 16:17:38 -0300

    listar apenas um registro

---
+++ listarUm.php
@@ -0,0 +1,28 @@
+<?php
+    try {
+      $conecta = mysqli_connect("localhost","id10374988_app","teste321","id10374988_app1");
+                              //servidor , usuario banco, senha, nome do banco
+
+      $codigo = $_POST['codigo'];
+      $query = "select * from tb_livro where cd_livro = $codigo";
+
+
+        $resultado = mysqli_query($conecta,$query);
+
+
+        while($linha = mysqli_fetch_assoc($resultado)){
+          $registro = array(
+              'livro'=>array(
+                'codigo' => $linha['cd_livro'],
+                'titulo' => $linha['nm_livro'],
+                'autor' => $linha['nm_autor'],
+                'ano' => $linha['ano'],
+              )
+          );
+        }
+
+       echo json_encode($registro);
+
+    } catch (Exception $e ) {
+        echo "Erro ao buscar: ".$e;
+    }
